016d7adcaa668546732b4a030bd11246798fc4d4
kevinmutwiri <keivnmutwiri@gmail.com>
2025-05-25 13:32:42 +0300

    Refactor - use repeat string method

--- 8-square.js
+++ 8-square.js
@@ -11,10 +11,6 @@ else
     size = parseInt(args[2]);
     for (let i = 0; i < size; i++)
     {
-        for (let j = 0; j < size; j++)
-        {
-            process.stdout.write('x');
-        }
-        console.log();
+        console.log('x'.repeat(size));
     }
 }
